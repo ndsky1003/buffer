555dafb92d6c81bec82e822517a6f1bc1addc51e
cc <cc@1t.com>
2024-11-14 21:26:21 +0700

    fix

--- buffer.go
+++ buffer.go
@@ -19,10 +19,21 @@ func Get() *buffer {
 	return pool.Get().(*buffer)
 }
 
+func Release(b *buffer) {
+	b.Release()
+}
+
 func (this *buffer) Release() {
 	if this == nil {
 		return
 	}
 	this.Reset()
 	pool.Put(this)
+}
+
+func (this *buffer) Bytes() []byte {
+	bs := this.Buffer.Bytes()
+	b := make([]byte, len(bs))
+	copy(b, bs)
+	return b
 }
